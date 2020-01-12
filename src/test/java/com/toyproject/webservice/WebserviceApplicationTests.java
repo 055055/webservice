2fee2e530da224d53bf51290efd76c39f9a9ba53
055055 <snb4301@gmail.com>
2020-01-12 22:51:52 +0900

    hellocontroller test add

---
+++ src/test/java/com/toyproject/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.webservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
