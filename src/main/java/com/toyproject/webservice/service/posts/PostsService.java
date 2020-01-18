b3301d77beee867d32d0e9157159dcdec383dd80
055055 <snb4301@gmail.com>
2020-01-18 18:40:47 +0900

    게시판 CRUD 기능 완성

--- src/main/java/com/toyproject/webservice/service/posts/PostsService.java
+++ src/main/java/com/toyproject/webservice/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.toyproject.webservice.service.posts;
 
 import com.toyproject.webservice.domain.posts.Posts;
 import com.toyproject.webservice.domain.posts.PostsRepository;
+import com.toyproject.webservice.web.dto.PostsListResponseDto;
 import com.toyproject.webservice.web.dto.PostsResponseDto;
 import com.toyproject.webservice.web.dto.PostsSaveRequestDto;
 import com.toyproject.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,16 @@ public class PostsService {
                 IllegalArgumentException("해당 사용자가 없 습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다..id="+id));
+        postsRepository.delete(posts);
+    }
+
 }
---
+++ src/main/java/com/toyproject/webservice/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.toyproject.webservice.web.dto;
+
+import com.toyproject.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entitiy) {
+        this.id = entitiy.getId();
+        this.title = entitiy.getTitle();
+        this.author = entitiy.getAuthor();
+        this.modifiedDate = entitiy.getModifiedDate();
+    }
+}
